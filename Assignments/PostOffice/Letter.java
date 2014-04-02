2b6e79614082bef3c7597bd403fe1da8ffc154fa
BigBoss424 <ajones061392@gmail.com>
2014-04-02 11:38:47 -0700

    Letter Class 1.0
    Created the EVC, getters and setters, toString, and getPostage methods
    into the class.

--- Assignments/PostOffice/Letter.java
+++ Assignments/PostOffice/Letter.java
@@ -1,11 +1,62 @@
 public class Letter
 {
 	private String tName;
-	private Address address;
+	private Address tAddress;
 	private String fName;
 	private Address fAddress;
 	private double weight; //in ounces
 	
-	public Letter()
-
+	public Letter(String tName, Address tAddress, 
+	String fName, Address fAddress, Double weight)
+	{
+		this.tName = tName;
+		this.fName = fName;
+		this.fAddress = fAddress;
+		this.tAddress = tAddress;
+		this.weight = weight;
+	}
+	
+	public String getTname(String tName)
+	{
+		return tName;
+	}
+	
+	public void setTname(String tName)
+	{
+		this.tName = tName;
+	}
+	
+	public String getFname(String fName)
+	{
+		return fName;
+	}
+	
+	public void setFname(String fName)
+	{
+		this.fName = fName;
+	}
+	
+	public double getWeight(double weight)
+	{
+		return weight;
+	}
+	
+	public void setWeight(double weight)
+	{
+		this.weight = weight;
+	}
+	
+	public static double getPostage(double weight)
+	{
+		double postage = .46;
+		postage = weight * postage;
+		return postage;
+	}
+	public String toString()
+	{
+		return "--------------------------------------------" + "\n" +
+			   "-------------" + "\n" + 
+			   "From: " + this.fName + "\n" + 
+			   this.tAddress.toString() + "\t\t\t"+ "Postage" + getPostage();
+	}
 }
