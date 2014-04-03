c38740c20359a07773729b278a208d6ff6f5d550
BigBoss424 <ajones061392@gmail.com>
2014-04-03 10:44:03 -0700

    Post Office day 2
    Created the tester class which handles main for the program. Added the
    folder Utilities to hold all the utilities such as FileUtil.

--- Assignments/PostOffice/PostOffice.java
+++ Assignments/PostOffice/PostOffice.java
@@ -1,8 +1,52 @@
+import java.util.Scanner;
+
 public class PostOffice
 {
-	public static void main(String [] args)
+	Letter[] letters = new Letter();
+	
+	private PostOffice()
 	{
-		System.out.println("Test");
+		
+	}
+	
+	public static void readLetters(String filename)
+	{
+		String sWork;
+		int iWork = 0;
+		int count = 0;
+		
+		//Create Strings for each variable
+		/*
+			Such as the address
+		*/
+		
+		while(fin.hasNext())
+		{
+			count++;
+			fin.nextLine();
+		}
+		fin.close();
+		
+		count = count / 7;
+		
+		ltrAra = new Letter[count];
+		
+		/*
+			to extract specific data from a file
+			toName = 
+		*/
+		
+		
+	}
+	
+	public static void sortLetters(String filename)
+	{
+	
+	}
+	
+	public static void printLetters()
+	{
+	
 	}
 
 }
