6d1fb9cec5ed11f55a4718b82eb8f71b221c938f
BigBoss424 <ajones061392@gmail.com>
2014-04-04 10:21:24 -0700

    Labs
    Started to work on Lab 1 and the Extra Credit Lab for this quarter. So
    far I just have a skeleton.

---
+++ Labs/APE practice/PgmDesign.java
@@ -0,0 +1,77 @@
+import java.io.*;
+import java.util.*;
+
+public class PgmDesign
+{
+	private static Scanner fin = null;
+	private static PrintStream output;
+	
+	private static boolean connectInput(String filename)
+	{
+	
+		return false;
+	}
+	
+	private static void disconnectInput()
+	{
+	
+	}
+	
+	private static void connectOutput(String filename)
+	{
+	
+	}
+	
+	private static void disconnectOutput()
+	{
+	
+	}
+	
+	private static String nextItem()
+	{
+	
+	
+	}
+	
+	private static int readData(String[] data)
+	{
+	
+	}
+	
+	public static void main(String[] args)
+	{
+		String[] input = ("IOin1.txt", "IOin2.txt");
+		String[] data = new String[256];
+		String output = "IOout.txt";
+		int j;
+		
+		connectOutput(output);
+		
+		for(j = 0; j < input.length;j++)
+		{
+			if(connectInput(input[j]))
+			{
+				int size = readData(data);
+				int k;
+				
+				System.out.println("Connection succeeded to " + input[j]);
+				for(k = 0; k < size; k++)
+				{
+					writeLine(data[k]);
+				}
+				disconnectInput();
+			}else
+			{
+				System.out.println("Connection failed to " + input[j]);
+			}
+		}
+		disconnectOutput();
+		System.out.println("Attempting to open WriteProtect.txt for output - should 
+		produce an error message.");
+		connectOutput("WriteProtect.txt");
+	
+	}
+	
+
+
+}
