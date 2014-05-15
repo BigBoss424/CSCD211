65db887bf1abb90b8e583805616ca770cf33fcce
BigBoss424 <ajones061392@gmail.com>
2014-05-15 11:03:04 -0700

    Added the required methods to make the InvoiceItem class work properly

--- Labs/DataAbstraction/InvoiceItem.java
+++ Labs/DataAbstraction/InvoiceItem.java
@@ -3,14 +3,22 @@
    {
     
    //  Add variable 'ouncesPerUnit' as an int...
-
+       protected int ouncesPerUnit;
 
 
    
    //  Add constructor.  
    //   Parameters are: int itemID, String description, double price, int quantityOrdered, int ouncesPerUnit
-
-
+      public InvoiceItem(int itemID, String description, double price, int quantityOrdered, int ouncesPerUnit)
+      {
+         super.itemID = itemID;
+         super.description = description;
+         super.price = price;
+         super.quantityOrdered = quantityOrdered;
+         this.ouncesPerUnit = ouncesPerUnit;
+      }
+    
+   
 
 
 
@@ -19,14 +27,22 @@
 	//   The method creates a String that reports the processing that would take place.
 	//   See example output in output.txt
 
-
+   @Override 
+   protected String changeInventory()
+   {
+      return "Inventory has been relieved for" + this.description + " by " + this.quantityOrdered;
+   }
 
 
    
    
    //  Override 'toString' method here.
 	//   Return 'toString' from Item class plus the ounces per unit...
-
+   @Override
+   public String toString()
+   {
+      return super.toString() + "Ounces: " + ouncesPerUnit + "\n";
+   }
 
 
 
