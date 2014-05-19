13514a0756746cea310432779df99ff39d645ce5
BigBoss424 <ajones061392@gmail.com>
2014-05-18 17:12:32 -0700

    Added the base case to the Recursion.java, it compiles but it doesn't output the result I want. Trying to figure out how to use recursion to compare individual index values.

--- Labs/Recursion/Part 1 - Array220/Recursion.java
+++ Labs/Recursion/Part 1 - Array220/Recursion.java
@@ -1,4 +1,10 @@
-
+/*  Write the 'array220' method in the 'Recursion' class.
+	*  Given an array of ints, compute recursively if the array 
+	*  contains somewhere a value followed in the array by that value times 10.
+	*  For example the array { 1, 3, 42, 7, 70, 0 } contains 7 followed 
+	*  immediately by 70 so your recursive method should return 'true'.
+	*  Be sure to handle an empty array.
+	*/
    public class Recursion
    {
    
@@ -8,8 +14,16 @@
       public static boolean array220(int[] nums, int index) 
       {	  
       //  Your code goes here...
-         
-      
+         int n = nums.length;
+         if(n == 0)
+         {
+            return false;
+         }
+         else if(nums[index] == nums[index*10])
+         {
+            return true;
+         }
+         return array220(nums, index);
       }
    
    
