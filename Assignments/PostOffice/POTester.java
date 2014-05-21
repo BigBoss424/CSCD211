aafaecf08081636010b86ae3bf69f835ed2510bb
BigBoss424 <ajones061392@gmail.com>
2014-05-21 00:03:42 -0700

    Added comments to POTester.java

--- Assignments/PostOffice/POTester.java
+++ Assignments/PostOffice/POTester.java
@@ -1,5 +1,12 @@
 import java.io.*;
-
+/*
+Created by: Aaron Jones
+Date: 5/20/14
+Description: An application that uses several class such as letter,
+address and post office to organize them in an array, and print the 
+letters from a text file. Along with how much it would postage would
+cost.
+*/
 public class POTester
 {
 
