7898135b08ff352bedae9a1a3a64eb1b7afcbed5
BigBoss424 <ajones061392@gmail.com>
2014-05-27 11:02:22 -0700

    Added the necessary files to test out the selection sort for a Linked List class

---
+++ Labs/Linked List 2/LinkedListTester.java
@@ -0,0 +1,12 @@
+public class LinkedListTester
+{
+
+   public static void main(String []args)
+   {
+      ListReferenceBased linkedlist = new ListReferenceBased();
+      
+      linkedlist.add(1);
+      linkedlist.add(2);
+   }
+
+}
