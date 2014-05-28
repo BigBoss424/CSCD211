fcd2f0341f2e71807ceb86169b69449a43e2653f
BigBoss424 <ajones061392@gmail.com>
2014-05-28 10:29:55 -0700

    Added all the files and folders from the Packages Lab. As well as the PDF version of the lab for ease of printing. And the PDF with the given instructions.

---
+++ Labs/Packages/tempFolder/CallingClass.java
@@ -0,0 +1,12 @@
+import pack1.CalledClass;
+
+public class CallingClass
+{
+	public static void main(String[] args)
+	{
+		int x = CalledClass.method1();
+		System.out.println(x);
+	}
+
+
+}
---
+++ Labs/Packages/tempFolder/pack1/CalledClass.java
@@ -0,0 +1,9 @@
+package pack1;
+public class CalledClass
+{
+	public static int method1()
+	{
+		return 1002;
+	}
+
+}
