eaec8281aa2ddc570175a4894bc7954cc987d11f
BigBoss424 <ajones061392@gmail.com>
2014-06-04 12:04:03 -0700

    Finished the Quiz2TryCatch class, so that I can study for the final exam by seeing what the output would be.

--- Examples/Quiz2TryCatch.java
+++ Examples/Quiz2TryCatch.java
@@ -18,8 +18,27 @@ public class Quiz2TryCatch
             System.out.println("AIOOBE in main.");
          }
       }catch(ArithmeticException e)
-
+      {
+         System.out.println("Arithmetic Error " + e.toString());
+      }
+      catch(Exception e)
+      {
+         System.out.println("Some Other Error " + e.toString());
+      }
+   }//end main
    
-   }
+   private static int anotherMethod()
+   {
+      int[] c = {1};
+      try
+      {
+         c[2] = 99;
+      }
+      catch(ArrayIndexOutOfBoundsException e)
+      {
+         System.out.println("AIOOBE in anotherMethod.");
+      }
+      return c[1];
+   }//end anotherMethod
 
-}
+}//end class
